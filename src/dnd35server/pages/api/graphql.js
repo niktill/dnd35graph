54fc54e71b0fcf01c552a0a0f833a0057fce8efc
Nik Till <niktill123@gmail.com>
2021-06-07 11:27:38 -0400

    fix cors

--- src/dnd35server/pages/api/graphql.js
+++ src/dnd35server/pages/api/graphql.js
@@ -13,15 +13,13 @@ export const config = {
   },
 };
 
-//const handler = apolloServer.createHandler({ path: '/api/graphql' });
-
 export default cors((req, res) => {
   if (req.method === 'OPTIONS') {
     res.end();
     return false;
   }
 
-  return server.createHandler({
+  return apolloServer.createHandler({
     path: '/api/graphql',
   })(req, res);
 });
